e528ee0be28dff3c09735fac0c77d02dde314360
Vitor Kruel102 <vkruel.programador@gmail.com>
2022-12-01 19:12:50 -0300

    Add migration.

--- Primeiro_projeto/core/models.py
+++ Primeiro_projeto/core/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    preco = models.DecimalField('Preço', ma)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
 

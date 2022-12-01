95e277952cd1eac389a03cc71378f36b04587376
Vitor Kruel102 <vkruel.programador@gmail.com>
2022-12-01 18:44:57 -0300

    Modificação da estrutura do Proejto.

--- Primeiro_projeto/site_first1/settings.py
+++ Primeiro_projeto/site_first1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q%5*o#ih@4ueqjl0tfz(agnm)zjh$=xe2xl!w6_nv$ft3ds^5k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'site_first1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 

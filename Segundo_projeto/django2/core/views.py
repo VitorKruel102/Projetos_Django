e1f86c71ca4f0ac3dcd96eced319f01828c5fa3d
Vitor Kruel102 <vkruel.programador@gmail.com>
2022-12-06 13:39:14 -0300

    Add formulario nos contatos.

--- Segundo_projeto/django2/core/views.py
+++ Segundo_projeto/django2/core/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
+from .forms import ContatoForm
 
 def index(request):
+    """."""
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    """."""
+    form = ContatoForm()
+
+    contex = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
+    """."""
     return render(request, 'produto.html')
